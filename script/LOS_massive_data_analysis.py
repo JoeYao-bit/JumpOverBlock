30e8dae2672c38bc7792f0cf2cb9a02ad936e3a5
yaozhuo <1521232476@qq.com>
2025-08-04 14:31:12 +0800

    update

--- script/LOS_massive_data_analysis.py
+++ script/LOS_massive_data_analysis.py
@@ -184,7 +184,7 @@ def drawCompareTimeCost(all_data, dim):
             # 计算移动平均
             window_size = math.ceil(len(x)/5)  # 控制平滑程度
             y_smooth = uniform_filter1d(y_sorted, size=window_size) # window_size must be integer
-            plt.plot(x_sorted, y_smooth, label=label_map_of_type[type_key]+'_width='+str(map_key))
+            plt.plot(x_sorted, y_smooth, linestyle=line_map_of_type[type_key], label=label_map_of_type[type_key]+'_width='+str(map_key))
             
             # coefficients = np.polyfit(x, y, deg=1)
             # trend_line = np.poly1d(coefficients)
@@ -294,7 +294,7 @@ def drawSBTInitData(all_data, dim):
             # 计算移动平均
             window_size = math.ceil(len(x)/5)  # 控制平滑程度
             y_smooth = uniform_filter1d(y_sorted, size=window_size) # window_size must be integer
-            plt.plot(x_sorted, y_smooth, label=label_map_of_type[type_key]+'_width='+str(map_key))
+            plt.plot(x_sorted, y_smooth, linestyle=line_map_of_type[type_key], label=label_map_of_type[type_key]+'_width='+str(map_key))
 
             # coefficients = np.polyfit(x, y, deg=1)
             # trend_line = np.poly1d(coefficients)
@@ -326,7 +326,7 @@ def drawSBTUpdateData(all_data, dim):
         if a_data.name not in map_data[a_data.max_obs_move_distance]:
             map_data[a_data.max_obs_move_distance][a_data.name] = dict()
 
-        if a_data.dimension_length not in map_data[a_data.name]:
+        if a_data.dimension_length not in map_data[a_data.max_obs_move_distance][a_data.name]:
             map_data[a_data.max_obs_move_distance][a_data.name][a_data.dimension_length] = dict()
             map_data[a_data.max_obs_move_distance][a_data.name][a_data.dimension_length]["occ_ratio"] = list()
             map_data[a_data.max_obs_move_distance][a_data.name][a_data.dimension_length]["update_time_cost"] = list()
@@ -334,14 +334,13 @@ def drawSBTUpdateData(all_data, dim):
         map_data[a_data.max_obs_move_distance][a_data.name][a_data.dimension_length]["occ_ratio"].append(a_data.occ_ratio)
         map_data[a_data.max_obs_move_distance][a_data.name][a_data.dimension_length]["update_time_cost"].append(a_data.update_time_cost)
 
-    fig=plt.figure(figsize=(5,4)) #添加绘图框
-    ax = plt.axes()
-    plt.ylabel("Update Time Cost(ms)", fontsize = 13)
-    plt.xlabel("Occ Ratio", fontsize = 13)    
-
     for max_dist, temp_value in map_data.items():
+        fig=plt.figure(figsize=(5,4)) #添加绘图框
+        ax = plt.axes()
+        plt.ylabel("Update Time Cost(ms)", fontsize = 13)
+        plt.xlabel("Occ Ratio", fontsize = 13)    
         for type_key, type_value in map_data[max_dist].items():
-            for map_key, map_value in map_data[max_dist][type_key].items():
+            for dim_length_key, map_value in map_data[max_dist][type_key].items():
                 x = map_value["occ_ratio"]
                 y = map_value["update_time_cost"]
                 scatter = ax.scatter(x, y, marker='.')
@@ -357,7 +356,7 @@ def drawSBTUpdateData(all_data, dim):
                 # 计算移动平均
                 window_size = math.ceil(len(x)/5)  # 控制平滑程度
                 y_smooth = uniform_filter1d(y_sorted, size=window_size) # window_size must be integer
-                plt.plot(x_sorted, y_smooth, label=label_map_of_type[type_key]+'_width='+str(map_key))
+                plt.plot(x_sorted, y_smooth, linestyle=line_map_of_type[type_key], label=label_map_of_type[type_key]+'_width='+str(dim_length_key))
 
                 # print('x/y size = ', len(x), " / ", len(y))
                 # coefficients = np.polyfit(x, y, deg=1)
@@ -388,6 +387,12 @@ label_map_of_type = {
     "SBT_RAW":"QUAD/OCT"
 }
 
+line_map_of_type = {
+    "RAW":"-",
+    "SBT":"--",
+    "SBT_RAW":"-."
+}
+
 file_path = "../test/SBT_LOS.txt"
 
 all_compare_data = loadDataFromfile(file_path)
@@ -397,6 +402,7 @@ drawSBTInitData(all_compare_data, 2)
 drawSBTUpdateData(all_compare_data, 2)
 
 drawCompareTimeCost(all_compare_data, 2)
+
 printStatistic(all_compare_data, 2)
 
 # drawSBTInitData(all_compare_data, 3)
