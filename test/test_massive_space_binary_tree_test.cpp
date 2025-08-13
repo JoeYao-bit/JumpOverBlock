9ade480260e9bb23a35d5574e078bf40e542d08f
Your Name <1521232476@qq.com>
2025-08-14 04:30:22 +0800

    update

--- test/test_massive_space_binary_tree_test.cpp
+++ test/test_massive_space_binary_tree_test.cpp
@@ -90,7 +90,7 @@ std::string file_path = "../test/SBT_LOS";
 // massif-visualizer massif.out
 int main() {
 //TEST(massiveSBTLOSCompareTest, test) {
-    for(int i=0; i<20; i++) {
+    for(int i=0; i<100; i++) {
 
 //        massiveSBTLOSCompareTest2D(10, 100, {200, 300, 400}, {10, 20, 40});
 //        massiveSBTLOSCompareTest<2, SpaceBinaryTree2D>(10, 100,
@@ -145,28 +145,28 @@ int main() {
         //                                              true,
         //                                              4);
 
-        int width_of_space = 800;
-        // massiveSBTLOSCompareTest<2>(1,
-        //                                              1,
-        //                                              {width_of_space},
-        //                                              {10,20,30,40,50,60,70,80},
-        //                                              file_path,
-        //                                              1e5,
-        //                                              1e3,
-        //                                              {2},
-        //                                              true,
-        //                                              4);
-
-        massiveSBTLOSCompareTest<3>(1,
-                                                     1,
-                                                     {width_of_space}, // {200, 400, 600, 800, 1000}
-                                                     {10, 40, 160, 320, 640, 960, 1280},
+        int width_of_space = 1000;
+        massiveSBTLOSCompareTest<2>(1,
+                                                     10,
+                                                     {width_of_space},
+                                                     {1, 5, 10, 20, 40, 80, 160},
                                                      file_path,
                                                      1e5,
                                                      1e3,
                                                      {2},
                                                      true,
                                                      4);
+
+        // massiveSBTLOSCompareTest<3>(1,
+        //                                              1,
+        //                                              {width_of_space}, // {200, 400, 600, 800, 1000}
+        //                                              {10, 40, 160, 320, 640, 960, 1280},
+        //                                              file_path,
+        //                                              1e5,
+        //                                              1e3,
+        //                                              {2},
+        //                                              true,
+        //                                              4);
 
     }
 }
