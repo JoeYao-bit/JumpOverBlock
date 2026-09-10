b3a6ad8060f9abf994976d8e2917ae66d8e5c2a5
yaozhuo <1521232476@qq.com>
2026-09-11 05:59:51 +0800

    update

--- algorithm/line_of_sight_jump_between_block.h
+++ algorithm/line_of_sight_jump_between_block.h
@@ -261,6 +261,15 @@ namespace freeNav::JOB {
         Line<T, 2> line(start, end);
         float pre_tMaxX = 1e20, pre_tMaxY = 1e20;
         bool step_by_step = false;
+        float b_min_x,b_min_y;
+        float b_max_x,b_max_y;
+        float t_x_low,t_y_low;
+        float t_x_high,t_y_high;
+        float delta_tx, delta_ty;
+        float t_block_exit;
+        float tNext;
+        float t_candidate_x = 1e20f;
+        float t_candidate_y = 1e20f;
         while (x >= 0 && x < gridW && y >= 0 && y < gridH)
         {
             //std::cout << "tMaxX/Y = " << tMaxX << "/" << tMaxY << std::endl;
@@ -289,15 +298,12 @@ namespace freeNav::JOB {
 
                 //std::cout << "block_max/min = " << block_max << "/" << block_min << std::endl;
 
-                float t_candidate_x = 1e20f;
-                float t_candidate_y = 1e20f;
-
                 if(std::fabs(dir[0]) > eps)
                 {
-                    float b_min_x = static_cast<float>(block_min[0]);
-                    float b_max_x = static_cast<float>(block_max[0]) + 1.0f;
-                    float t_x_low  = (b_min_x - start[0]) / dir[0];
-                    float t_x_high = (b_max_x - start[0]) / dir[0];
+                    b_min_x = static_cast<float>(block_min[0]);
+                    b_max_x = static_cast<float>(block_max[0]) + 1.0f;
+                    t_x_low  = (b_min_x - start[0]) / dir[0];
+                    t_x_high = (b_max_x - start[0]) / dir[0];
                     if(dir[0] > 0)
                         t_candidate_x = t_x_high;
                     else
@@ -306,10 +312,10 @@ namespace freeNav::JOB {
 
                 if(std::fabs(dir[1]) > eps)
                 {
-                    float b_min_y = static_cast<float>(block_min[1]);
-                    float b_max_y = static_cast<float>(block_max[1]) + 1.0f;
-                    float t_y_low  = (b_min_y - start[1]) / dir[1];
-                    float t_y_high = (b_max_y - start[1]) / dir[1];
+                    b_min_y = static_cast<float>(block_min[1]);
+                    b_max_y = static_cast<float>(block_max[1]) + 1.0f;
+                    t_y_low  = (b_min_y - start[1]) / dir[1];
+                    t_y_high = (b_max_y - start[1]) / dir[1];
                     if(dir[1] > 0)
                         t_candidate_y = t_y_high;
                     else
@@ -335,7 +341,6 @@ namespace freeNav::JOB {
                 y = static_cast<int>(std::floor(exit_pt[1]));
 
                 // ========== 增量式更新全局 tMaxX、tMaxY ==========
-                float delta_tx, delta_ty;
                 if (dir[0] > 0.f)
                     delta_tx = (static_cast<float>(x + 1) - exit_pt[0]) * tDeltaX;
                 else
@@ -351,7 +356,7 @@ namespace freeNav::JOB {
                 // ================================================
             } else {
                 // 下一步跨网格的最小t值
-                float tNext = std::min(tMaxX, tMaxY);
+                tNext = std::min(tMaxX, tMaxY);
                 // 到达终点区间，直接退出，不再前进
                 if (tNext >= 1.f - eps)
                     return false;
@@ -385,7 +390,7 @@ namespace freeNav::JOB {
         Pointi<2> current_pt;
         Pointf<2> current_fpt;
         Id current_id;
-
+        Pointi<2> pt;
         // 1. 起点终点完全重合
         if (std::fabs(dir[0]) < eps && std::fabs(dir[1]) < eps)
         {
@@ -393,7 +398,8 @@ namespace freeNav::JOB {
             int x0 = static_cast<int>(std::floor(start[0]));
             int y0 = static_cast<int>(std::floor(start[1]));
             if (x0 >= 0 && x0 < gridW && y0 >= 0 && y0 < gridH) {
-                if(isoc(Pointi<2>{x0, y0})) {
+                pt[1] = x0; pt[1] = y0;
+                if(isoc(pt)) {
                     return true;
                 }
             }
@@ -401,6 +407,7 @@ namespace freeNav::JOB {
         }
 
         // 2. 纯垂直线：x全程不变，只遍历y轴
+        BlockPtr<2> current_block = nullptr;
         if (std::fabs(dir[0]) < eps) {
             //std::cout << "case 1 " << std::endl;
             int fixedX = static_cast<int>(std::floor(start[0]));
@@ -415,11 +422,11 @@ namespace freeNav::JOB {
                 if (fixedX >= 0 && fixedX < gridW && y >= 0 && y < gridH)
                 {
                     current_pt = Pointi<2>{fixedX, y};
-                    if(isoc(Pointi<2>{fixedX, y})) {
+                    if(isoc(current_pt)) {
                         return true;
                     }
                     current_id = PointiToId(current_pt, block_detector_ptr->dimension_info_);
-                    BlockPtr<2> current_block = block_detector_ptr->block_ptr_map_[current_id];
+                    current_block = block_detector_ptr->block_ptr_map_[current_id];
                     if(current_block != nullptr) {
                         if (current_block->max_[1]+1 > yEnd) {
                             return false;
@@ -450,11 +457,11 @@ namespace freeNav::JOB {
                 if (x >= 0 && x < gridW && fixedY >= 0 && fixedY < gridH)
                 {
                     current_pt = Pointi<2>{x, fixedY};
-                    if(isoc(Pointi<2>{x, fixedY})) {
+                    if(isoc(current_pt)) {
                         return true;
                     }
                     current_id = PointiToId(current_pt, block_detector_ptr->dimension_info_);
-                    BlockPtr<2> current_block = block_detector_ptr->block_ptr_map_[current_id];
+                    current_block = block_detector_ptr->block_ptr_map_[current_id];
                     if(current_block != nullptr) {
                         if (current_block->max_[0]+1 > xEnd) {
                             return false;
@@ -495,6 +502,15 @@ namespace freeNav::JOB {
 
         float pre_tMaxX = 1e20, pre_tMaxY = 1e20;
         bool step_by_step = false;
+        float b_min_x,b_min_y;
+        float b_max_x,b_max_y;
+        float t_x_low,t_y_low;
+        float t_x_high,t_y_high;
+        float delta_tx, delta_ty;
+        float t_block_exit;
+        float tNext;
+        float t_candidate_x = 1e20f;
+        float t_candidate_y = 1e20f;
         while (x >= 0 && x < gridW && y >= 0 && y < gridH) {
             //std::cout << "tMaxX/Y = " << tMaxX << "/" << tMaxY << std::endl;
             step_by_step = false;
@@ -513,21 +529,20 @@ namespace freeNav::JOB {
                 return true;
             }
             current_id = PointiToId(current_pt, block_detector_ptr->dimension_info_);
-            BlockPtr<2> current_block = block_detector_ptr->block_ptr_map_[current_id];
+            current_block = block_detector_ptr->block_ptr_map_[current_id];
             if (!step_by_step && current_block != nullptr) {
                 Pointi<2> block_min = current_block->min_;
                 Pointi<2> block_max = current_block->max_; // the real boundary of current block
 
                 //std::cout << "block_max/min = " << block_max << "/" << block_min << std::endl;
 
-                float t_candidate_x = 1e20f;
-                float t_candidate_y = 1e20f;
+
 
                 if(std::fabs(dir[0]) > eps) {
-                    float b_min_x = static_cast<float>(block_min[0]);
-                    float b_max_x = static_cast<float>(block_max[0]) + 1.0f;
-                    float t_x_low  = (b_min_x - start[0]) / dir[0];
-                    float t_x_high = (b_max_x - start[0]) / dir[0];
+                    b_min_x = static_cast<float>(block_min[0]);
+                    b_max_x = static_cast<float>(block_max[0]) + 1.0f;
+                    t_x_low  = (b_min_x - start[0]) / dir[0];
+                    t_x_high = (b_max_x - start[0]) / dir[0];
                     if(dir[0] > 0)
                         t_candidate_x = t_x_high;
                     else
@@ -535,17 +550,17 @@ namespace freeNav::JOB {
                 }
 
                 if(std::fabs(dir[1]) > eps) {
-                    float b_min_y = static_cast<float>(block_min[1]);
-                    float b_max_y = static_cast<float>(block_max[1]) + 1.0f;
-                    float t_y_low  = (b_min_y - start[1]) / dir[1];
-                    float t_y_high = (b_max_y - start[1]) / dir[1];
+                    b_min_y = static_cast<float>(block_min[1]);
+                    b_max_y = static_cast<float>(block_max[1]) + 1.0f;
+                    t_y_low  = (b_min_y - start[1]) / dir[1];
+                    t_y_high = (b_max_y - start[1]) / dir[1];
                     if(dir[1] > 0)
                         t_candidate_y = t_y_high;
                     else
                         t_candidate_y = t_y_low;
                 }
 
-                float t_block_exit = std::min(t_candidate_x, t_candidate_y);
+                t_block_exit = std::min(t_candidate_x, t_candidate_y);
 
                 //std::cout << "t_block_exit = " << t_block_exit << std::endl;
 
@@ -563,7 +578,6 @@ namespace freeNav::JOB {
                 y = static_cast<int>(std::floor(exit_pt[1]));
 
                 // ========== 增量式更新全局 tMaxX、tMaxY ==========
-                float delta_tx, delta_ty;
                 if (dir[0] > 0.f)
                     delta_tx = (static_cast<float>(x + 1) - exit_pt[0]) * tDeltaX;
                 else
@@ -579,7 +593,7 @@ namespace freeNav::JOB {
                 // ================================================
             } else {
                 // 下一步跨网格的最小t值
-                float tNext = std::min(tMaxX, tMaxY);
+                tNext = std::min(tMaxX, tMaxY);
                 // 到达终点区间，直接退出，不再前进
                 if (tNext >= 1.f - eps)
                 return false;
